c0e4f8de77615c7dd51713a29fd6648dc6463d22
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-07-30 07:18:15 +0000

    [update]一覧、詳細、削除[add]usersコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
+
+  resources :books, only: [:index, :create, :show, :destroy]
   get 'homes/about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

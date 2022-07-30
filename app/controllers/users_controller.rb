0323ab9aec1e4b35d8b9b8dc7b3662584084bb6f
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-07-30 15:30:07 +0000

    [update]各ページリンク先変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,38 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new
     @users = User.all
   end
-  
+
   def show
+    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to books_path
   end
 
   def edit
      @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
     redirect_to user_path(user.id)
   end
-  
+
   private
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :create, :edit, :update]
   get 'homes/about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

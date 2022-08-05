a12fdda1e097d51a554cf5dddf3056ee14f4f437
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-08-05 14:44:09 +0000

    [update]部分テンプレート導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,16 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(book.id), notice: 'You have created book successfully.'
+      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
     else
+      @user = current_user
       @books = Book.all
       render :index
     end
@@ -18,6 +20,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.new
     @showbook = Book.find(params[:id])
+    @user = @showbook.user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index
     @book = Book.new
     @users = User.all
+    @user = current_user
   end
 
   def show

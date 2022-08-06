793205e37ac08698d71b86e6ead10cadeeab7758
EC2 Default User <ec2-user@ip-172-31-16-143.ap-northeast-1.compute.internal>
2022-08-06 17:16:24 +0000

    [update]自己紹介文にはバリデーションつけない

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   validates :name, presence: true
-  validates :introduction, presence: true
 
   def get_profile_image
     unless profile_image.attached?
